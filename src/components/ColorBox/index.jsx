a41bbbdaaa0b91cd26c28d7a17f5b823da8e2b48
Hung Developer <hungntse01@gmail.com>
2021-09-07 06:12:16 +0700

    ColorBox useState()

---
+++ src/components/ColorBox/index.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import './ColorBox.scss'
+
+ColorBox.propTypes = {
+
+};
+
+function getRandomColor() {
+    const COLOR_LIST = ['deeppink', 'green', 'yellow', 'orange'];
+    const randomIndex = Math.trunc(Math.random() * 4);
+    return COLOR_LIST[randomIndex];
+}
+
+
+function ColorBox() {
+    const [color, setColor] = useState(() => {
+        const initColor = localStorage.getItem('box_color') || 'deeppink';
+        return initColor;
+    });
+
+    function handleBoxClick() {
+        // get random color -> set color
+        const newColor = getRandomColor();
+        setColor(newColor);
+        localStorage.setItem('box_color', newColor);
+    }
+
+    return (
+        <div className="color-box" style={{ backgroundColor: color }} onClick={handleBoxClick}>
+            COLOR BOX
+        </div>
+    );
+}
+
+export default ColorBox;
